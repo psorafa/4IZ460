9db480c1acf43399c5719aed22e86cfd1ecc209d
Barbora's Mac <sourkbar@fel.cvut.cz>
2021-12-06 17:43:46 +0100

    ještě trochu refaktor a výstup pro otázku 2

--- models/q2.py
+++ models/q2.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import os
 
 # 2.	Jaké faktory nejvíce korelují s faktem, že se skladba stala hitem?
 
 # Pro zodpovězení těto otázky použijeme vlasností nejlepšího modelu klasifikátoru Random Forest.
 
-all_tracks = pd.read_csv("./output_data/dataset_categorised.csv")
+all_tracks = pd.read_csv("./output_data/dataset_categorised_2.csv")
 
 features = ['danceability', 'energy', 'key', 'loudness',
        'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness',
@@ -29,4 +30,8 @@ all_songs_feat_vars.columns = ['Variable', 'Feature importance']
 all_songs_feat_vars = all_songs_feat_vars.set_index('Variable')
 all_songs_feat_vars = all_songs_feat_vars.sort_values(by=['Feature importance'], ascending = False)
 
-print(all_songs_feat_vars)
+# print(all_songs_feat_vars)
+outpath = os.path.join(os.path.dirname(__file__) + os.sep + 'q2', "q2_features.txt")
+outfile = open(outpath, "w")
+outfile.writelines(all_songs_feat_vars.to_string(header=True, index=True))
+outfile.close()
